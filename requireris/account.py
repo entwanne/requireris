4173297940d94eec63fb8b70bc74b5688490496e
Antoine Rozo <antoine.rozo@gmail.com>
2025-06-25 19:18:26 +0200

    Use ini file instead of csv as database

--- requireris/account.py
+++ requireris/account.py
@@ -1,21 +1,29 @@
-import csv
+from configparser import ConfigParser, UNNAMED_SECTION
 from base64 import b32decode
 
 from . import exceptions
 
 
 def get_accounts(filename):
-    with open(filename) as csvfile:
-        reader = csv.reader(csvfile, delimiter=':')
-        return {name:key.replace(' ', '').upper() for (name, key) in reader}
-    return None
+    config = ConfigParser(allow_unnamed_section=True)
+    config.read(filename)
+    if not config.has_section(UNNAMED_SECTION):
+        return {}
+    values = config[UNNAMED_SECTION]
+    return {
+        name: key.replace(' ', '').upper()
+        for name, key in values.items()
+    }
 
 
 def update_accounts(accounts, filename):
-    with open(filename, 'w') as csvfile:
-        writer = csv.writer(csvfile, delimiter=':')
-        for name, key in accounts.items():
-            writer.writerow([name, key])
+    config = ConfigParser(allow_unnamed_section=True)
+    config.read_string('a=b')
+    config.remove_option(UNNAMED_SECTION, 'a')
+    for name, key in accounts.items():
+        config.set(UNNAMED_SECTION, name, key)
+    with open(filename, 'w') as f:
+        config.write(f)
 
 
 def add_accounts(names, key, filename):
